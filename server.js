65a7d48dba2bdc416ce7258f78c0ddfc05eb7394
Remo Inderbitzin <remo.inderbitzin@siemens.com>
2016-12-30 18:37:44 +0100

    fix: reformat

--- server.js
+++ server.js
@@ -1,45 +1,63 @@
-var express     =   require("express");
-var app         =   express();
-var server      =   require("http").createServer(app);  
-var io          =   require("socket.io")(server);
-var router      =   express.Router();
-var bodyParser  =   require("body-parser");
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+var router = express.Router();
+var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({"extended" : false}));
+app.use(bodyParser.urlencoded({
+    "extended": false
+}));
 
-io.on('connection', function(client){
+io.on('connection', function (client) {
     console.log("WebSocket connection established");
+    client.on('ledon', function (data) {
+        console.log("WebSocket: ledon arrived");
+        io.emit('ledon', data);
+    });
+
+    client.on('ledoff', function (data) {
+        console.log("WebSocket: ledon arrived");
+        io.emit('ledoff', data);
+    });
+
 });
 
-router.get("/",function(req,res){
-    // res.json({"message" : "Welcome to nodejs-websocket-server"});
-    res.sendFile(__dirname + '/index.html');
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 
 });
 
 router.route("/ledon")
-    .post(function(req,res){
+    .post(function (req, res) {
         var ledid = req.body.ledid;
         var ledcolor = req.body.ledcolor;
-        var response = {"ledid" : ledid, "message" : "TurnOn", "ledcolor": ledcolor};
+        var response = {
+            "ledid": ledid,
+            "message": "TurnOn",
+            "ledcolor": ledcolor
+        };
 
-        io.emit('ledon',response);        
+        io.emit('ledon', response);
         res.json(response);
         console.log(response);
     });
 
 router.route("/ledoff")
-    .post(function(req,res){
+    .post(function (req, res) {
         var ledid = req.body.ledid;
-        var response = {"ledid" : ledid, "message" : "TurnOff"};
+        var response = {
+            "ledid": ledid,
+            "message": "TurnOff"
+        };
 
-        io.emit('ledoff',response);        
+        io.emit('ledoff', response);
         res.json(response);
         console.log(response);
     });
 
-app.use('/',router);
+app.use('/', router);
 
-server.listen(8080);
+server.listen(3000);
 console.log("Listening to PORT 8080");
