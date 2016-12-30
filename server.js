eb4a37c9b8f402a1f51cc15d23b16a8861b45841
Remo Inderbitzin <remo.inderbitzin@siemens.com>
2016-12-30 18:43:42 +0100

    fix: path

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ io.on('connection', function (client) {
 });
 
 router.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(__dirname + '/index.html');
 
 });
 
